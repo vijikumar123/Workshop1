b5e97bdea3a7a7f2ad56081124d7d7104d51443c
vijikumar123 <vk9164940756@gmail.com>
2023-01-13 21:50:41 +0530

    added: workshop1problems

---
+++ src/com/bridgelabz/primenumber/FibonacciSeries.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.primenumber;
+
+public class FibonacciSeries {
+    public static void main(String[] args)
+    {
+        int fib1=10;
+        int fib2=8;
+        int 
+
+    }
+}
